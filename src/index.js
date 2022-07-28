d5b581c7fcb3faae28fd20606f3f38f536baebf6
NBojan <63514753+NBojan@users.noreply.github.com>
2022-07-27 19:10:09 -0500

    finalise

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 import { GitProvider } from "./context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-//const key = "dev-y31-wfyv.us.auth0.com";
-//const clientId = "Q1cNRarOvRu5feeFTRf7PVwcRdkr2E4f";
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
