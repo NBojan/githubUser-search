8030ff5f2f78cafb2c346168cb70252c227c1718
NBojan <63514753+NBojan@users.noreply.github.com>
2022-07-27 19:09:07 -0500

    trying

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import { GitProvider } from "./context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const key = "dev-y31-wfyv.us.auth0.com";
-const clientId = "Q1cNRarOvRu5feeFTRf7PVwcRdkr2E4f";
+//const key = "dev-y31-wfyv.us.auth0.com";
+//const clientId = "Q1cNRarOvRu5feeFTRf7PVwcRdkr2E4f";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={key}
-      clientId={clientId}
+      domain={`${process.env.REACT_APP_DOMAIN_KEY}`}
+      clientId={`${process.env.REACT_APP_CLIENT_ID}`}
       redirectUri={window.location.origin}
       cacheLocation= "localstorage"
     >
